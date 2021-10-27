532e75e9d3dff8d198dc073c053ef519887538c4
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-10-27 14:22:03 +0300

    add dwmblocks config.def.h file

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    {"",        "updates",         3600,            3}, 
+    {"",        "disk-space",      240,             5},
+    {"",        "mem-usage",       3,		        8},
+    {"",        "gpu-temp",        3,               20}, 
+    {"",        "cpu-temp",        3,               11}, 
+	{"",        "bar-date ",       30,		        55},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " │ ";
+static unsigned int delimLen = 5;
